9673d20475ba3ecd3460e13c720613dcc8e9c903
GM Ishtiaq Anwar Abir <gm.abir.1415@gmail.com>
2025-12-13 01:47:13 +0600

    Map Vercel Postgres variables

--- config/database.php
+++ config/database.php
@@ -85,13 +85,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'),
+            // 👇 These lines allow it to find the Vercel Database 👇
+            'host' => env('DB_HOST', env('POSTGRES_HOST', '127.0.0.1')),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'database' => env('DB_DATABASE', env('POSTGRES_DATABASE', 'forge')),
+            'username' => env('DB_USERNAME', env('POSTGRES_USER', 'forge')),
+            'password' => env('DB_PASSWORD', env('POSTGRES_PASSWORD', '')),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
